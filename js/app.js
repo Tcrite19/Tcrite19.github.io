da177d3636752a602c9959cbdacb77355f8773c3
Tcrite19 <crite2004@gmail.com>
2024-04-04 01:32:00 -0400

    feat: add styling for menu icon and navbar for 768 pixels and add toggle functionality

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 /*==================== toggle icon navbar ====================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onClick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 /*==================== scroll sections active link ====================*/
 
 /*==================== sticky navbar ====================*/
-/*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
 let header = document.querySelector('header');
 header.classList.toggle('sticky', window.scrollY > 100);
+
+/*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 /*==================== scroll reveal ====================*/
 /*==================== typed js ====================*/
 /*const body = document.querySelector('body');
